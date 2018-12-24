04227ff8f6fcae8cb31e517ccbfd8e3375f0a7d7
suriamit <suri_amit@hotmail.com>
2018-12-24 15:58:31 -0500

    Add files via upload

---
+++ Dashboard/DataStore/DataManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using Microcharts;
+
+namespace Dashboard.DataStore
+{
+    public class DataManager
+    {
+        public DataManager()
+        {
+        }
+
+        internal static List<Entry> GetOSList()
+        {
+            return
+                new List<Entry>
+            {
+                new Entry(200) {
+                    Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                    Label = "Windows 7",
+                    ValueLabel = "44953"
+                },
+                new Entry(100) {
+                    Color = SkiaSharp.SKColor.Parse("#00BFFF"),
+                    Label = "Windows 10",
+                    ValueLabel = "13003"
+                },
+                new Entry(150) {
+                    Color = SkiaSharp.SKColor.Parse("#00CED1"),
+                    Label = "Windows 2008",
+                    ValueLabel = "3995"
+                }
+            };
+        }
+
+        internal static List<Entry> GetDesktopList()
+        {
+            return
+                new List<Entry>
+            {
+                new Entry(44939) {
+                    Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                    Label = "Physical Desktop",
+                    ValueLabel = "44939"
+                },
+                new Entry(13706) {
+                    Color = SkiaSharp.SKColor.Parse("#00BFFF"),
+                    Label = "Laptop",
+                    ValueLabel = "13706"
+                },
+                new Entry(4202) {
+                    Color = SkiaSharp.SKColor.Parse("#00CED1"),
+                    Label = "Virtual Desktop",
+                    ValueLabel = "4202"
+                }
+            };
+        }
+
+        internal static List<Entry> GetBrowserList()
+        {
+            return
+                new List<Entry>
+            {
+                new Entry(44939) {
+                    Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                    Label = "IE11",
+                    ValueLabel = "65110"
+                },
+                new Entry(13706) {
+                    Color = SkiaSharp.SKColor.Parse("#00BFFF"),
+                    Label = "Chrome",
+                    ValueLabel = "43429"
+                },
+                new Entry(4202) {
+                    Color = SkiaSharp.SKColor.Parse("#2c3e50"),
+                    Label = "IE10",
+                    ValueLabel = "24"
+                }
+            };
+        }
+
+        internal static List<Entry> GetComplianceTrends()
+        {
+            return
+                new List<Entry>
+            {
+                new Entry(93) {
+                    Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                    Label = "Mon",
+                    ValueLabel = "93%"
+                },
+                new Entry(92) {
+                    Color = SkiaSharp.SKColor.Parse("#00BFFF"),
+                    Label = "Tue",
+                    ValueLabel = "92.5%"
+                },
+                new Entry(96) {
+                    Color = SkiaSharp.SKColor.Parse("#3498db"),
+                    Label = "Wed",
+                    ValueLabel = "96%"
+                },
+                new Entry(94) {
+                    Color = SkiaSharp.SKColor.Parse("#77d065"),
+                    Label = "Thu",
+                    ValueLabel = "94%"
+                },
+                new Entry(95) {
+                    Color = SkiaSharp.SKColor.Parse("#b455b6"),
+                    Label = "Fri",
+                    ValueLabel = "95%"
+                }
+            };
+        }
+    }
+}
---
+++ Dashboard/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dashboard.DataStore;
+using Microcharts;
+using Xamarin.Forms;
+using Entry = Microcharts.Entry;
+
+namespace Dashboard
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            chartOS.Chart = new RadialGaugeChart
+            {
+                Entries = DataManager.GetOSList(),
+                LabelTextSize = 12
+            };
+            chart3.Chart = new BarChart { 
+                Entries = DataManager.GetDesktopList(),
+                PointSize = 4,
+                LabelTextSize = 12
+            };
+            chart4.Chart = new DonutChart { Entries = DataManager.GetBrowserList() };
+
+
+            chartBit9.Chart = new PointChart { Entries = DataManager.GetComplianceTrends() };
+            chartMcAfee.Chart = new RadarChart { Entries = DataManager.GetComplianceTrends() };
+            chartTanium.Chart = new LineChart
+            {
+                Entries = DataManager.GetComplianceTrends(),
+                LineSize = 4,
+                PointSize = 4,
+                LabelTextSize = 12
+
+            };
+        }
+    }
+}
